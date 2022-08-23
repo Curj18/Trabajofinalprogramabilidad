1e67026589a3a309f9eaad7085df043921d4c9a7
jorge <jorgecurilao@gmail.com>
2022-08-23 17:59:22 -0400

    tenant y VRF

--- Creacion_de_Tenant.py
+++ Creacion_de_Tenant.py
@@ -6,3 +6,8 @@ from acitoolkit.acitoolkit import *
 session = Session(URL, LOGIN, PASSWORD)
 session.login()
 
+#codigo necesario para la creacion de tenant y VRF
+tenant_name = "Tenant_de_telefonica"
+tenant = Tenant(tenant_name)
+vrf = Context("Example_VRF", tenant)
+
